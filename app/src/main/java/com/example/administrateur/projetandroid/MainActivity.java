1b93c42798347e292d50fc448a61eb6f4541a4c2
RaphaelIENCO <raphaelienco@gmail.com>
2019-03-23 17:55:10 +0100

    Ajout settings

--- app/src/main/java/com/example/administrateur/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/administrateur/projetandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrateur.projetandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -68,8 +69,11 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
