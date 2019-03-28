ef0dd0dc230fe5351be49a0eb78058632383b0dd
RaphaelIENCO <raphaelienco@gmail.com>
2019-03-28 23:44:17 +0100

    Distance affichage

--- app/src/main/java/com/example/administrateur/projetandroid/MapFragment.java
+++ app/src/main/java/com/example/administrateur/projetandroid/MapFragment.java
@@ -176,6 +176,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
                 myMarker = new MarkerOptions().position(vous).title(sharedPref.getString(getResources().getString(R.string.key_name),"John Smith"));
                 gMap.addMarker(myMarker);
 
+                for(Restaurant r: restaurantList){
+                    String radiusValue = sharedPref.getString(getResources().getString(R.string.key_search_radius),
+                            "123");
+                    double radius = Double.parseDouble(radiusValue);
+
+                    if (distance(r.getLatitude(), maLat, r.getLongitude(), maLong)<=radius) {
+                        LatLng pos = new LatLng(r.getLatitude(),r.getLongitude());
+                        myMarker = new MarkerOptions().position(pos).title(r.getNom());
+                        gMap.addMarker(myMarker);
+                    }
+
+                }
+
                 gMap.moveCamera(CameraUpdateFactory.newLatLng(vous));
                 gMap.moveCamera(CameraUpdateFactory.zoomTo(10));
             }
@@ -243,13 +256,23 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
 
-        for(Restaurant r: restaurantList){
-            LatLng pos = new LatLng(r.getLatitude(),r.getLongitude());
-            myMarker = new MarkerOptions().position(pos).title(r.getNom());
-            gMap.addMarker(myMarker);
 
-        }
 
+    }
+    public static double distance(double lat1, double lat2, double lon1,
+                                  double lon2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        return Math.sqrt(distance);
     }
 
     /**
